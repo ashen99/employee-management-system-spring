cba19bf65b2e6eace7fa1d043f17893cf7e57fe5
ashen <ashenperera51@yahoo.com>
2023-09-26 09:43:52 +0530

    add emplyee

--- src/main/java/com/ashen/employeeManagement/service/EmployeeService.java
+++ src/main/java/com/ashen/employeeManagement/service/EmployeeService.java
@@ -5,10 +5,25 @@ import com.ashen.employeeManagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    public Employee addEmployee(Employee employee){
+        employee.setId(UUID.randomUUID().toString().split("-")[0]);
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepository.findAll();
+    }
+
     
+
+
+
 }
